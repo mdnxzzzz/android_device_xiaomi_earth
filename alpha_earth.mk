5bfa770e8846615841d913ef4b5d90ccc0ab0039
Medina <dmedinarivera12@gmail.com>
2025-11-05 00:07:15 +0100

    Update and rename lineage_earth.mk to alpha_earth.mk

--- alpha_earth.mk
+++ alpha_earth.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2023-2024 The LineageOS Project
+lineageageCopyright (C) 2023-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,9 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_earth
+PRODUCT_NAME :alpha_earth
 PRODUCT_DEVICE := earth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
